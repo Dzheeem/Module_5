d28e358830a88f380f933231ef185da2f6015f09
Dzhamil <Dzheeem@yandex.ru>
2024-10-09 16:40:01 +0700

    task finalized

--- module_5_3.py
+++ module_5_3.py
@@ -39,17 +39,19 @@ class House:
             return self.number_of_floors != other.number_of_floors
         return NotImplemented
 
-    def __add__(self, value):
-        if isinstance(value, int):
-            return House(self.name, self.number_of_floors + value)
-        return NotImplemented
+    def __add__(self, other):
+        if isinstance(other, House):
+            return House(self.name, self.number_of_floors + other.number_of_floors)
+        elif isinstance(other, int):
+            return House(self.name, self.number_of_floors + other)
 
 
     def __iadd__(self, other):
         if isinstance(other, House):
             self.number_of_floors += other.number_of_floors
-            return self
-        return NotImplemented
+        elif isinstance(other, int):
+            self.number_of_floors += other
+        return self
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -61,20 +63,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
